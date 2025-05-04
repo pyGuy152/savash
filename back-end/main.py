6b149b4ab18b17bb7e51f2ebae39a31567ebd867
pyGuy152 <sarveshsabe@gmail.com>
2025-05-04 16:30:30 -0500

    classroom init

--- back-end/main.py
+++ back-end/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
-from .routes import auth, users
+from .routes import auth, users, classroom
 
 app = FastAPI()
 
 app.include_router(auth.router)
 app.include_router(users.router)
+app.include_router(classroom.router)
 
 @app.get("/")
 def root():
     return {'message':'hello people of the world'}
+
