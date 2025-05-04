98d406bf1749bfd8ea8d2313bb0f526f3f400e95
pyGuy152 <sarveshsabe@gmail.com>
2025-05-04 17:02:32 -0500

    updated schema

--- back-end/schemas.py
+++ back-end/schemas.py
@@ -1,16 +1,22 @@
 from typing import Optional
 from pydantic import BaseModel, EmailStr
+from enum import Enum
 
 class LoginInput(BaseModel):
     email: str
     password: str
+
+
+class UserRole(str, Enum):
+    STUDENT = "student"
+    TEACHER = "teacher"
 
 class UserCreate(BaseModel):
     name: str
     username: str
     email: EmailStr
     password: str
-    role: str 
+    role: UserRole
 
 class UserOut(BaseModel):
     username: str
