3daf0bb890b188edc578c9bba3a3a0261f95219b
pyGuy152 <sarveshsabe@gmail.com>
2025-05-04 16:54:28 -0500

    allow frontend

--- back-end/main.py
+++ back-end/main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
 from .routes import auth, users, classroom
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    'https://savash.rohanjain.xyz',
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(auth.router)
 app.include_router(users.router)
