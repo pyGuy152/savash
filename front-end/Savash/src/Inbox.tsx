af743d9d7a3be36dae815b530068669e0f0b3d5a
RanMC-9918 <rohanjain0725@gmail.com>
2025-05-05 22:20:44 -0500

    get build ready

--- front-end/Savash/src/Inbox.tsx
+++ front-end/Savash/src/Inbox.tsx
@@ -1,56 +1,52 @@
-
 import "./Inbox.css";
 import MessageList from "./components/MessageList";
 
-import StudentNav from "./components/StudentNav"
+import StudentNav from "./components/StudentNav";
 import TeacherNav from "./components/TeacherNav";
 
 import { useState, useEffect } from "react";
 import { getToken } from "./types.ts";
 
-let apiUrl = "https://api.codewasabi.xyz"
+let apiUrl = "https://api.codewasabi.xyz";
 
 function Inbox() {
-    const [messages, setMessages] = useState<string[]>(["Loading"]);
+  const [messages, setMessages] = useState<Number[]>([]);
 
-    function loadMessages() {
-      fetch(apiUrl + "/users", {
-        method: "GET",
-        headers: {
-          Authorization: "bearer " + getToken(document.cookie)
-        }
-      })
-      .then(res => res.json())
-      .then(data => {
-        if(!data || data.join_req.length == 0){
+  function loadMessages() {
+    fetch(apiUrl + "/users", {
+      method: "GET",
+      headers: {
+        Authorization: "bearer " + getToken(document.cookie),
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (!data || data.join_req.length == 0) {
           setMessages([]);
           return;
         }
-        console.log(data)
-        setMessages(data.join_req);
-      })
-    }
+        console.log(data);
+        setMessages(data.join_req.map((elem: string) => Number(elem)));
+      });
+  }
 
-    useEffect(loadMessages, []);
+  useEffect(loadMessages, []);
 
-    if(localStorage.getItem("role") == 'teacher'){
-        return (
-          <>
-            <TeacherNav />
-            <MessageList messages={messages} />
-          </>
-        );
-    }
-    else{
-        return (
-          <>
-            <StudentNav />
-            <MessageList messages={messages} />
-          </>
-        );
-    }
-
-    
+  if (localStorage.getItem("role") == "teacher") {
+    return (
+      <>
+        <TeacherNav />
+        <MessageList messages={messages} />
+      </>
+    );
+  } else {
+    return (
+      <>
+        <StudentNav />
+        <MessageList messages={messages} />
+      </>
+    );
+  }
 }
 
 export default Inbox;
