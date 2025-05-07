fb4bd2326873e048baf0ab0a67642377683e427b
pyGuy152 <sarveshsabe@gmail.com>
2025-05-06 19:04:19 -0500

    add messages route with initial GET endpoint

---
+++ back-end/routes/post.py
@@ -0,0 +1,32 @@
+from pickletools import int4
+from typing import List
+from fastapi import APIRouter, status, HTTPException, Depends
+import psycopg2, os, time
+from psycopg2 import Error
+from psycopg2.extras import RealDictCursor
+from dotenv import load_dotenv
+from .. import schemas, utils, oauth2
+
+load_dotenv()
+db_user = os.getenv("DB_User")
+db_pass = os.getenv("DB_User_PASS")
+
+# connect and create a cursor for the db
+while True:
+    try:
+        conn = psycopg2.connect(database='savash',user=db_user,password=db_pass,host='localhost',port='5432',cursor_factory=RealDictCursor)
+        cur = conn.cursor()
+        break
+    except Error as e:
+        if conn:
+            conn.close()
+        print(f"Error connecting to DB: {e}")
+        time.sleep(5)
+
+
+router = APIRouter(prefix="/messages",tags=['Messages'])
+
+@router.get('/')
+def get_post():
+    return {'messages':'wasup'}
+
