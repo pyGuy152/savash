2c6eb11fdf6d29dfff877c96fcc20721016d4535
pyGuy152 <sarveshsabe@gmail.com>
2025-05-25 21:16:49 -0500

    you now have to be authorized to run python code

--- back-end/app/routes/run.py
+++ back-end/app/routes/run.py
@@ -6,7 +6,7 @@ import os
 router = APIRouter(prefix='/run',tags=['Run Code'])
 
 @router.post("/")
-async def run_python_code(file : UploadFile = File(...)):
+async def run_python_code(file : UploadFile = File(...), tokenData = Depends(oauth2.get_current_user)):
     if not 'python' in str(file.content_type):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Please send a python file")
     contents = await file.read()
