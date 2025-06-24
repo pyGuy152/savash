09f4c3df4d89255d85095dca6fc16ea967572888
RanMC-9918 <rohanjain0725@gmail.com>
2025-06-24 11:41:12 -0500

    changes game page to link to real game

--- front-end/Savash/src/Game.tsx
+++ front-end/Savash/src/Game.tsx
@@ -1,54 +1,19 @@
 import { useEffect } from "react";
 import "./Game.css";
+import HomeNav from "./components/HomeNav";
 
 function Game() {
-  useEffect(() => {
-    setTimeout(() => {
-      const module = document.createElement("script");
-      module.type = "importmap";
-      module.innerHTML = `
-        {
-          "imports": {
-            "three": "https://cdn.jsdelivr.net/npm/three/build/three.module.js",
-            "three/examples/jsm/loaders/GLTFLoader.js": "https://cdn.jsdelivr.net/npm/three@0.155.0/examples/jsm/loaders/GLTFLoader.min.js"
-          }
-        }
-        `;
-      document.body.appendChild(module);
-
-      const gameScript = document.createElement("script");
-      gameScript.type = "module";
-      gameScript.src = "/game/Game.js";
-      document.body.appendChild(gameScript);
-
-      const setupScript = document.createElement("script");
-      setupScript.type = "module";
-      setupScript.src = "/game/setup.js";
-      document.body.appendChild(setupScript);
-
-      const cameraScript = document.createElement("script");
-      cameraScript.type = "module";
-      cameraScript.src = "/game/camera.js";
-      document.body.appendChild(cameraScript);
-    }, 100);
-  }, []);
 
 
 
   return (
     <>
-      <div id="root-window">
-        <div id="loading">LOADING...</div>
-        <div id="start">
-          <button id="host">
-            Host
-          </button>
-          <button id="join">
-            Join
-          </button>
+    <HomeNav />
+      <a href="https://savashkart.rohanjain.xyz/">
+        <div id='savash-img'>
+          <h2>The Savash Game</h2>
         </div>
-        <div id="render-target"></div>
-      </div>
+      </a>
     </>
   );
 }
